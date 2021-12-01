4ccbf85175008763e810e27c1cf5ae3bda78a18e
khuntkaran <khuntkaran995@gmail.com>
2021-12-01 20:43:01 +0530

    Create oddorevennumber.c

---
+++ c-languag/02-if-else/02-if-else/oddorevennumber.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main (){
+	int a,b;
+	printf("Enter tha a number\n");
+	scanf("%d",&a);
+	
+	if(a%2==0){
+		printf("%d is even\n",a);
+	}
+	
+	else{
+		printf("%d is odd\n",a);
+	}
+	
+	
+}
