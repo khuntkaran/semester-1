33dc2a862b8568852c6da676857c39519124f54b
khuntkaran <khuntkaran995@gmail.com>
2021-12-01 20:44:07 +0530

    Create norp-number2.c

---
+++ c-languag/02-if-else/02-if-else/norp-number2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    float a;
+    printf("enter a = ");
+    scanf("%f",&a);
+    if (a>0)
+    {
+        printf("a is positive");
+    }
+    else
+    {
+        printf("a is negative");
+    }
+    
+}
