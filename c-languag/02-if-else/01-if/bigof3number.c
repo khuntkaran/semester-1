a05567facb90d9d160838fd859d09e11fb38ee59
khuntkaran <khuntkaran995@gmail.com>
2021-12-01 20:43:50 +0530

    Create bigof3number.c

---
+++ c-languag/02-if-else/01-if/bigof3number.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main ()
+{
+	int a;
+	int b;
+    int c;
+	
+	printf("a number is\n");
+	scanf("%d",&a);
+	
+	printf("b number is \n");
+	scanf("%d",&b);
+
+    printf("c number is \n");
+	scanf("%d",&c);
+	
+	if(a>b && a>c)
+    {
+		printf("%d a number is big\n",a);
+	}
+	if (b>c && b>a)
+    {
+        printf("%d b number is big\n",b); 
+    }
+    if (c>b && c>a)
+    {
+        printf("%d c number is big\n",c);
+    }
+	
+}
