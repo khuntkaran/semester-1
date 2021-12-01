a2f270a4d5d8fe4c52c11ad2cfc2f56c791f19b8
khuntkaran <khuntkaran995@gmail.com>
2021-12-01 20:43:04 +0530

    Create big2number4.c

---
+++ c-languag/02-if-else/05-conditional-operators/big2number4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+
+	printf("enter a = ");
+	scanf("%d",&a);
+
+	printf("enter b = ");
+	scanf("%d",&b);
+
+	(a>b)?(printf("number a is big")):(printf("number b is big"));
+}
