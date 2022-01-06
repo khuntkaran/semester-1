1f31eb6816830f69483c0fd972e2db482fcd4e9f
khuntkaran <21010101108@darshan.ac.in>
2022-01-06 18:45:43 +0530

    Update replace.c

--- c-languag/05-string/replace.c
+++ c-languag/05-string/replace.c
@@ -1,18 +1,27 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 void main()
 {
-    int a;
-    char name[10];
+    int a, i;
+    char name[10], c;
 
     printf("enter your name = ");
     gets(name);
-   
-   
+
     printf("wich number of replace character = ");
-    scanf("%d",&a);
+    scanf("%d", &a);
+    fflush(stdin);
     printf("what is new character = ");
-    scanf("%s",&name[a-1]);
-    
-    printf("name is %s",name); 
+
+    scanf("%c", &c);
+
+    for (i = 0; i < a; i++)
+    {
+        if (i == a - 1)
+        {
+            name[a - 1] = c;
+        }
+    }
+
+    printf("name is %s", name);
 }
